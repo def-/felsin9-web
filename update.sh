ffcbdc0cb75f402f28b814f5061e2ff7aa53be74
Dennis Felsing <dennis@felsin9.de>
2023-06-05 13:36:16 +0200

    felsin9.de -> felsing.org

--- update.sh
+++ update.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 set -e
-mount /mnt/www-1und1 || true
 cd www
 jekyll build
-rsync -rlptoDvP _site/ /mnt/www-1und1/nnis
-fusermount -u /mnt/www-1und1 || true
+rsync -rlptoDvP _site/ root@ddnet:/var/www-felsing-dennis
